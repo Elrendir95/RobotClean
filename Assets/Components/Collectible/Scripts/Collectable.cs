5316b6fbee5e8791dd65f1f3f93528dc5ad60213
Elrendir <gouhie.l@gmail.com>
2026-03-08 23:10:45 +0100

    Add a log if a collectable is not on the correct layer

--- Assets/Components/Collectible/Scripts/Collectable.cs
+++ Assets/Components/Collectible/Scripts/Collectable.cs
@@ -1,7 +1,16 @@
+using System;
 using UnityEngine;
 
 public abstract class Collectable : MonoBehaviour
 {
+    private void Start()
+    {
+        if (gameObject.layer != LayerMask.NameToLayer("Collectable"))
+        {
+            Debug.LogError($"{gameObject.name} collectable not affected on the correct layer");
+        }
+    }
+
     public virtual void OnCollect(GameObject collector)
     {
         Destroy(gameObject);
