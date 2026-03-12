0e1b514a1f251cf1e5f2028f23ad4a56ce4c93e6
Elrendir <gouhie.l@gmail.com>
2026-03-12 22:13:28 +0100

    Add Disctance Counting systems

---
+++ Assets/Scripts/Manager/DistanceManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Library.References;
+using UnityEngine;
+
+namespace Manager
+{
+    public class DistanceManager : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField] private FloatReference runtimeSpeed;
+        [SerializeField] private FloatReference runDistance;
+
+        private void Update()
+        {
+            runDistance.Value += runtimeSpeed.Value * Time.deltaTime;
+        }
+    }
+}
