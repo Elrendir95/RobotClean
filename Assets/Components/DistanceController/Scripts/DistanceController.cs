2a78fb7119152528af00ba5f0b05fa6f43c8b1f6
Elrendir <gouhie.l@gmail.com>
2026-03-12 22:14:18 +0100

    Convert Testing Scrips for Speed & Distances a components for the builds

---
+++ Assets/Components/DistanceController/Scripts/DistanceController.cs
@@ -0,0 +1,17 @@
+﻿using Library.References;
+using UnityEngine;
+
+namespace Components
+{
+    public class DistanceController : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField] private FloatReference runtimeSpeed;
+        [SerializeField] private FloatReference runDistance;
+
+        private void FixedUpdate()
+        {
+            runDistance.Value += runtimeSpeed.Value * Time.fixedDeltaTime;
+        }
+    }
+}
---
+++ Assets/Components/SpeedController/Scripts/SpeedController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using Components.EventSystem;
+using Library.References;
+using UnityEngine;
+
+namespace Components
+{
+    public class SpeedController : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField] private FloatReference startSpeed;
+        [SerializeField] private FloatReference maxSpeed;
+        [SerializeField] private FloatReference runtimeSpeed;
+        [SerializeField] private FloatReference increaseSpeedTime;
+        [SerializeField] private FloatReference increaseSpeedAmount;
+
+        private void OnEnable()
+        {
+            Events.OnLifeCountChanged += OnLifeCountChanged;
+        }
+
+        private void OnDisable()
+        {
+            Events.OnLifeCountChanged -= OnLifeCountChanged;
+        }
+
+        private void OnLifeCountChanged(float currentLife)
+        {
+            if (currentLife > 0) return;
+            runtimeSpeed.Value = 0;
+            gameObject.SetActive(false);
+        }
+
+        private void Start()
+        {
+            runtimeSpeed.Value = startSpeed.Value;
+            StartCoroutine(SpeedCoroutine());
+        }
+
+        IEnumerator SpeedCoroutine()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(increaseSpeedTime.Value);
+                if (runtimeSpeed.Value > maxSpeed.Value) continue;
+                runtimeSpeed.Value += increaseSpeedAmount.Value;
+                runtimeSpeed.Value = Mathf.Clamp(runtimeSpeed.Value, startSpeed, maxSpeed.Value);
+            }
+        }
+    }
+}
