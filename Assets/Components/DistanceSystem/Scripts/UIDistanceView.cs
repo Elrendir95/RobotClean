7a21c957c67bf6f5a0e39403b62dc98c37dabd9b
Elrendir <gouhie.l@gmail.com>
2026-03-12 22:40:55 +0100

    Rename DistanceController and SpeedController directory Add HUD elements for DistanceControlle Add Test Scene for SpeedController and DistanceController Add Both systeme in the mergedSystemeScene

---
+++ Assets/Components/DistanceSystem/Scripts/UIDistanceView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Components.EventSystem;
+using Library.References;
+using TMPro;
+using UnityEngine;
+
+namespace Components
+{
+    public class UIDistanceView :  MonoBehaviour
+    {
+        [SerializeField] private TMP_Text distanceText;
+        [SerializeField] private FloatReference distanceReference;
+
+        private void OnEnable()
+        {
+            distanceReference.OnValueChanged.AddListener(UpdateView);
+        }
+
+        private void OnDisable()
+        {
+            distanceReference.OnValueChanged.RemoveListener(UpdateView);
+        }
+
+        private void Start()
+        {
+            UpdateView(distanceReference.Value);
+        }
+
+        private void UpdateView(float newDistance)
+        {
+            distanceText.text = newDistance.ToString("0");
+        }
+    }
+}
