3befbf4fa9552c25e2b97f581ed9ce65a42e11ba
Elrendir <gouhie.l@gmail.com>
2026-03-21 17:24:28 +0100

    Finishing MainMenu

---
+++ Assets/Components/MainMenu/Scripts/UIMainMenuController.cs
@@ -0,0 +1,22 @@
+﻿using Components.SceneLoader;
+using UnityEngine;
+
+namespace Components.MainMenu
+{
+    public class UIMainMenuController :  MonoBehaviour
+    {
+        public void NewGame()
+        {
+            SceneLoaderService.LoadGame();
+        }
+
+        public void QuitGame()
+        {
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #else
+            Application.Quit();
+            #endif
+        }
+    }
+}
---
+++ Assets/Components/SceneLoader/SceneLoaderService.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace Components.SceneLoader
+{
+    public static class SceneLoaderService
+    {
+        public static void LoadMainMenu()
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+
+        public static void LoadGame()
+        {
+            SceneManager.LoadScene("Game",  LoadSceneMode.Single);
+            SceneManager.LoadScene("HUD", LoadSceneMode.Additive);
+        }
+    }
+}
