28ba0eb6ee30bb4d074c71f54e6b2d62c0d91dc3
Elrendir <gouhie.l@gmail.com>
2026-04-02 00:12:39 +0200

    Implements ScriptableObjectDatabase - Use it in CountDown state to play countdown SFX

---
+++ Assets/Components/ScriptableObjectDatabase/Scripts/ScriptableObjectDatabase.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Components.AudioSystem;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Components
+{
+    public static class ScriptableObjectDatabase
+    {
+        private static readonly Dictionary<Type, Dictionary<string, Object>> Database = new();
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void Initialize()
+        {
+            Database.Clear();
+
+            Register<AudioSO>();
+        }
+
+        private static void Register<T>() where T : Object
+        {
+            var type = typeof(T);
+
+            if (Database.ContainsKey(type))
+            {
+                Debug.LogWarning($"Trying to register duplicate type {type}");
+                return;
+            }
+
+            Database[type] = new Dictionary<string, Object>();
+
+            T[] templates = Resources.LoadAll<T>("");
+            foreach (var template in templates)
+            {
+                Database[type][template.name] = template;
+            }
+
+            Debug.Log($"Registered new type {type}");
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="name"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T Get<T>(string name) where T : Object
+        {
+            var type = typeof(T);
+
+            if (Database.TryGetValue(type, out var dictionary))
+            {
+                if (dictionary.TryGetValue(name, out var obj))
+                {
+                    return obj as T;
+                }
+            }
+
+            Debug.LogError("Unable to find a scriptable object with name " + name + " of type " + type);
+            return null;
+        }
+
+        public static List<T> GetAll<T>() where T : Object
+        {
+            var result = new List<T>();
+
+            var type = typeof(T);
+
+            if (Database.TryGetValue(type, out var dictionary))
+            {
+                // Equivalent of a foreach loop
+                result.AddRange(dictionary.Values.Select(obj => obj as T));
+            }
+            return result;
+        }
+    }
+}
--- Assets/Components/StateMachine/Scripts/States/CountDownState.cs
+++ Assets/Components/StateMachine/Scripts/States/CountDownState.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using Components.AudioSystem;
+using UnityEngine;
+using Components.EventSystem;
 
 namespace Components.StateMachine.States
 {
@@ -7,27 +9,58 @@ namespace Components.StateMachine.States
         private float _initialTime = 3f;
 
         public float Timer { get; private set; } = 0f;
+        private int _intTimer = 0;
 
         public CountDownState(StateMachine stateMachine) : base(stateMachine) {}
 
+        private AudioSO _tickSFX;
+        private AudioSO _goSFX;
+
         public override void Enter()
         {
+            _tickSFX = ScriptableObjectDatabase.Get<AudioSO>("countdownTickSFX");
+            _goSFX =  ScriptableObjectDatabase.Get<AudioSO>("countdownEndSFX");
+
+            PlayTickSFX();
+
             Timer = _initialTime;
+            _intTimer = Mathf.FloorToInt(_initialTime);
         }
 
         public override void Update()
         {
             Timer -= Time.deltaTime;
+
             if (Timer > 0f)
             {
-
+                var newIntTimer = Mathf.FloorToInt(Timer);
+                Debug.Log($"CountDown {Timer} : {_intTimer} != {newIntTimer}");
+                if (_intTimer != newIntTimer)
+                {
+                    PlayTickSFX();
+                    _intTimer = newIntTimer;
+                }
                 return;
             }
+
+            if (_goSFX != null)
+            {
+                Events.PlayAudio?.Invoke(_goSFX);
+            }
+
             // Countdown finished go to GameState
             var gameState = new GameState(StateMachine);
             StateMachine.ChangeState(gameState);
         }
 
         public override void Exit()  {}
+
+        private void PlayTickSFX()
+        {
+            if (_tickSFX != null)
+            {
+                Events.PlayAudio?.Invoke(_tickSFX);
+            }
+        }
     }
 }
