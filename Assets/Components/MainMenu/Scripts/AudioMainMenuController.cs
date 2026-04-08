a5d6110fe986b07e5e52e768a8c4ecf2f621401b
Elrendir <gouhie.l@gmail.com>
2026-04-08 22:12:04 +0200

    Add AudioController for Main Menu

---
+++ Assets/Components/MainMenu/Scripts/AudioMainMenuController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Components.AudioSystem;
+using Components.EventSystem;
+using UnityEngine;
+
+namespace Components.MainMenu
+{
+    public class AudioMainMenuController : MonoBehaviour
+    {
+        [SerializeField] private AudioSO clickSound;
+        [SerializeField] private AudioSO musicLoop;
+
+        private void Awake()
+        {
+            if (!musicLoop.config.loop)
+            {
+                Debug.LogWarning("Music not a loop!");
+            }
+
+            if (clickSound.config.loop)
+            {
+                Debug.LogWarning("Sound is a loop!");
+            }
+        }
+
+        public void Start()
+        {
+            Events.PlayAudio?.Invoke(musicLoop);
+        }
+
+        public void PlayClickSound()
+        {
+            Events.PlayAudio?.Invoke(clickSound);
+        }
+    }
+}
