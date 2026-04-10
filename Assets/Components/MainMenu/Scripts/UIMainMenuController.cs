3edb30df3979de5746e03f261ddfb94a4b43e48c
Elrendir <gouhie.l@gmail.com>
2026-04-11 00:16:25 +0200

    Fix main menu bug, after using pause.

--- Assets/Components/MainMenu/Scripts/UIMainMenuController.cs
+++ Assets/Components/MainMenu/Scripts/UIMainMenuController.cs
@@ -17,11 +17,18 @@ namespace Components.MainMenu
 
         private void Start()
         {
+            // Initialize timeScale to 1, in case of we used Pause menu
+            Time.timeScale = 1;
             _saveData = SaveService.Load();
             highScoreText.text = _saveData.highScore.ToString();
             highScoreWindow.SetActive(_saveData.highScore > 0);
         }
 
+        /// <summary>
+        /// Coroutine used to delay the scene loader
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
         IEnumerator ExecuteDelayCoroutine(Action action)
         {
             yield return new WaitForSeconds(execDelay);
@@ -31,7 +38,6 @@ namespace Components.MainMenu
         public void NewGame()
         {
             StartCoroutine(ExecuteDelayCoroutine(SceneLoaderService.LoadGame));
-
         }
 
         public void QuitGame()
