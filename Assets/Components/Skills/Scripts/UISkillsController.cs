1be54e6d2a08165e57dc304d4d8c097e26eabfaa
Elrendir <gouhie.l@gmail.com>
2026-04-15 22:13:38 +0200

    Add comment in UISkillsController.cs and add missing unsubscribe

--- Assets/Components/Skills/Scripts/UISkillsController.cs
+++ Assets/Components/Skills/Scripts/UISkillsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Components.EventSystem;
 using Components.SaveSystem;
@@ -14,6 +15,31 @@ namespace Components.Skills
         private SaveData _saveData;
         private readonly Dictionary<string, UISkillCard> _skillCards = new();
 
+        private void Start()
+        {
+            Events.BuySkill += BuySkill;
+
+            _saveData = SaveService.Load();
+
+            var skillList = ScriptableObjectDatabase.GetAll<Skill>();
+            foreach (var skill in skillList)
+            {
+                // Initialize Skill levels from saved data
+                skill.SetLevel(GetSkillLevelFromSaveData(skill.name));
+                // Create Skill card for UI
+                var card = Instantiate(skillCard, content.transform, false);
+                // Setup the card
+                card.Setup(skill);
+                // Keep track of the card to update it later
+                _skillCards.Add(skill.name, card);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            Events.BuySkill -= BuySkill;
+        }
+
         private int GetSkillLevelFromSaveData(string skillName)
         {
             // Look for the skill Level in the SaveData, return 0 if not found, the level otherwise
@@ -22,9 +48,11 @@ namespace Components.Skills
 
         private SaveSkill GetSkillFromSaveData(string skillName)
         {
+            // Get the first skill matching the name from the save
             var skill = _saveData.skills.FirstOrDefault(s => s.Name == skillName);
             if (skill == null)
             {
+                // No saved information, create a new one
                 skill = new SaveSkill();
                 skill.Name = skillName;
                 skill.Level = 0;
@@ -32,34 +60,26 @@ namespace Components.Skills
             return skill;
         }
 
-        private void Start()
-        {
-            Events.BuySkill += BuySkill;
-
-            _saveData = SaveService.Load();
-            var skillList = ScriptableObjectDatabase.GetAll<Skill>();
-            foreach (var skill in skillList)
-            {
-                skill.SetLevel(GetSkillLevelFromSaveData(skill.name));
-                var card = Instantiate(skillCard, content.transform, false);
-                card.Setup(skill);
-                _skillCards.Add(skill.name, card);
-            }
-        }
-
         public void BuySkill(string skillName)
         {
+            // Retrieve the reference data from the database
             var skillToImprove = ScriptableObjectDatabase.Get<Skill>(skillName);
 
+            // Check if the player can afford the next level
             if (_saveData.electronicsComponents <= skillToImprove.NextLevel.Cost) return;
 
+            // Update the save data
             var skill = GetSkillFromSaveData(skillName);
             _saveData.skills.Remove(skill);
             skill.Level++;
             _saveData.skills.Add(skill);
+
+            // Deduct the cost of the skill and save
             _saveData.electronicsComponents -= skillToImprove.NextLevel.Cost;
-            skillToImprove.SetLevel(skill.Level);
             SaveService.Save(_saveData);
+
+            // Update runtime objects and trigger refresh UI
+            skillToImprove.SetLevel(skill.Level);
             _skillCards[skillName].Setup(skillToImprove);
             Events.UpdateElectronicsUI?.Invoke(_saveData.electronicsComponents);
         }
